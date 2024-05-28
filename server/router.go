06b475ebf423551a989924728755636ef84c957f
Muhammad Ulil 'Azmi <ulilazmi100@gmail.com>
2024-05-29 02:07:27 +0700

    feat: add routings
    add routings for admin and user authentications, image upload, merchant and product/item management, routing with authentication or certain user authentication

---
+++ server/router.go
@@ -0,0 +1,95 @@
+package server
+
+import (
+	configs "beli_mang/cfg"
+	"beli_mang/controller"
+	"beli_mang/middleware"
+	"beli_mang/repo"
+	"beli_mang/svc"
+	"context"
+
+	"log"
+
+	awsCfg "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/labstack/echo/v4"
+)
+
+func (s *Server) RegisterRoute(config configs.Config) {
+	mainRoute := s.app.Group("")
+
+	registerAdminRoute(mainRoute, s.dbPool)
+	registerUserRoute(mainRoute, s.dbPool)
+	registerMerchantRoute(mainRoute, s.dbPool)
+	registerImageRoute(mainRoute, config)
+}
+
+func registerAdminRoute(r *echo.Group, db *pgxpool.Pool) {
+	ctr := controller.NewUserController(svc.NewUserSvc(repo.NewUserRepo(db)))
+
+	adminGroup := r.Group("/admin")
+	newRoute(adminGroup, "POST", "/register", ctr.AdminRegister)
+	newRoute(adminGroup, "POST", "/login", ctr.AdminLogin)
+}
+
+func registerUserRoute(r *echo.Group, db *pgxpool.Pool) {
+	ctr := controller.NewUserController(svc.NewUserSvc(repo.NewUserRepo(db)))
+
+	user := r.Group("/users")
+	newRouteWithAdminAuth(user, "POST", "/register", ctr.UserRegister)
+	newRoute(user, "POST", "/login", ctr.UserLogin)
+}
+
+func registerMerchantRoute(r *echo.Group, db *pgxpool.Pool) {
+	ctr := controller.NewMerchantController(svc.NewMerchantSvc(repo.NewMerchantRepo(db)))
+
+	merchantGroup := r.Group("/admin/merchants")
+	newRouteWithAdminAuth(merchantGroup, "POST", "", ctr.RegisterMerchant)
+	newRouteWithAdminAuth(merchantGroup, "GET", "", ctr.GetMerchant)
+	newRouteWithAdminAuth(merchantGroup, "POST", "/:merchantId/items", ctr.RegisterItem)
+	newRouteWithAdminAuth(merchantGroup, "GET", "/:merchantId/items", ctr.GetItem)
+}
+
+func registerImageRoute(r *echo.Group, config configs.Config) {
+	bucket := config.AWS_S3_BUCKET_NAME
+
+	// Load AWS configuration
+	cfg, err := awsCfg.LoadDefaultConfig(
+		context.Background(),
+		awsCfg.WithRegion(config.AWS_REGION),
+		awsCfg.WithCredentialsProvider(
+			credentials.NewStaticCredentialsProvider(
+				config.AWS_ACCESS_KEY_ID,
+				config.AWS_SECRET_ACCESS_KEY,
+				"",
+			),
+		),
+	)
+	if err != nil {
+		log.Fatalf("failed to load AWS config: %v", err)
+	}
+
+	// Initialize the image service and controller
+	imageService := svc.NewImageSvc(cfg, bucket)
+	imageController := controller.NewImageController(imageService)
+
+	// Register the route with authentication middleware
+	newRouteWithAdminAuth(r, "POST", "/image", imageController.UploadImage)
+}
+
+func newRoute(router *echo.Group, method, path string, handler echo.HandlerFunc) {
+	router.Add(method, path, handler)
+}
+
+func newRouteWithAuth(router *echo.Group, method, path string, handler echo.HandlerFunc) {
+	router.Add(method, path, handler, middleware.Auth)
+}
+
+func newRouteWithAdminAuth(router *echo.Group, method, path string, handler echo.HandlerFunc) {
+	router.Add(method, path, handler, middleware.AdminAuth)
+}
+
+func newRouteWithUserAuth(router *echo.Group, method, path string, handler echo.HandlerFunc) {
+	router.Add(method, path, handler, middleware.UserAuth)
+}

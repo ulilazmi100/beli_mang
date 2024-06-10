7f881b07233c607fdb5c9004d012f28e7a97bc1b
Muhammad Ulil 'Azmi <ulilazmi100@gmail.com>
2024-06-10 12:42:49 +0700

    chore: remove middlewares to alleviate server load

--- server/server.go
+++ server/server.go
@@ -26,12 +26,7 @@ func NewServer(db *pgxpool.Pool) *Server {
 
 	// Middleware
 	app.Use(middleware.Recover())
-	app.Use(middleware.RequestID())
 	app.Use(middleware.Logger())
-	app.Use(middleware.CORS())
-	app.Use(middleware.GzipWithConfig(middleware.GzipConfig{
-		Level: 5,
-	}))
 
 	return &Server{
 		dbPool:    db,

524b5206c6815f5f76405bdc7b79c4f0de97fdeb
Muhammad Ulil 'Azmi <ulilazmi100@gmail.com>
2024-06-11 12:14:34 +0700

    refactor: changes get nearby merchants strategy into using library functions and update related indexing
    note: PostGIS wasn't chosen because the one chosen was deemed lighter and more suitable (currently, PostGIS might be chosen later)

--- db/migrations/20240524153208_add_merchant_table.down.sql
+++ db/migrations/20240524153208_add_merchant_table.down.sql
@@ -4,4 +4,10 @@ DROP INDEX IF EXISTS idx_merchants_location;
 DROP INDEX IF EXISTS idx_merchants_category;
 DROP INDEX IF EXISTS idx_merchants_name;
 
+DROP INDEX IF EXISTS idx_ll_to_earth;
+
 DROP TABLE IF EXISTS merchants;
+
+-- Drop extensions
+DROP EXTENSION IF EXISTS "earthdistance";
+DROP EXTENSION IF EXISTS "cube";
--- db/migrations/20240524153208_add_merchant_table.up.sql
+++ db/migrations/20240524153208_add_merchant_table.up.sql
@@ -1,3 +1,7 @@
+-- Add extensions
+CREATE EXTENSION IF NOT EXISTS cube;
+CREATE EXTENSION IF NOT EXISTS earthdistance;
+
 -- Creating the merchants table
 CREATE TABLE IF NOT EXISTS merchants (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -10,9 +14,7 @@ CREATE TABLE IF NOT EXISTS merchants (
 );
 
 -- Creating an index on the merchants table for geolocation searches
-CREATE INDEX IF NOT EXISTS idx_merchants_location ON merchants USING gist (
-    point(latitude, longitude)
-);
+CREATE INDEX idx_ll_to_earth ON merchants USING gist(ll_to_earth(latitude, longitude));
 
 -- Creating additional indices for optimization
 -- Index on merchants for category and name filtering

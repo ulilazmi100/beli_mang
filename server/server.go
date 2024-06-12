4c80f3bf78f126cb081a0120f6b5e30b13384579
Muhammad Ulil 'Azmi <ulilazmi100@gmail.com>
2024-06-12 15:54:40 +0700

    feat: increase concurrency limit and add ppof

--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/pprof"
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	configs "beli_mang/cfg"
@@ -19,13 +20,14 @@ type Server struct {
 func NewServer(db *pgxpool.Pool) *Server {
 	app := fiber.New(fiber.Config{
 		Prefork:      false, // Disable prefork as it may increase memory usage per process (originally it's disabled by default though)
-		Concurrency:  50,    // Adjust this based on testing, e.g., start with 10
+		Concurrency:  305,   // Adjust this based on testing, e.g., start with 10
 		ErrorHandler: local_mid.ErrorHandler,
 	})
 
 	validate := validator.New()
 
 	app.Use(logger.New())
+	app.Use(pprof.New())
 
 	return &Server{
 		dbPool:    db,
